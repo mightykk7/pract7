de54e245414578cddf70f99a420a0efdb3433c6c
mightykk7 <ratovskijmaks@gmail.com>
2025-10-22 11:43:53 +0500

    Add files via upload

---
+++ zad7/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad7
+{
+    class User
+    {
+        public string Name { get; set; } = "Имя пользователя";
+        public string LastName { get; set; } = "Фамилия пользователя";
+        public string MiddleName { get; set; } = "Отчество пользователя";
+        public string Specialisation { get; set; } = "Специальность пользователя";
+        public string Password { get; set; } = "Пароль пользователя";
+    }
+}
